873415bd012d994763df78f6166316a7206da924
Peter-ChengTszTung <chengtsztung.peter@gmail.com>
2023-11-07 16:17:21 +0800

    Try langchain vectorstores

--- app/helper.py
+++ app/helper.py
@@ -1,5 +1,8 @@
 from langchain.chat_models import AzureChatOpenAI
 from langchain.chat_models.base import BaseChatModel
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.base import Embeddings
+
 import config
 
 
@@ -13,3 +16,15 @@ def make_chat_model() -> BaseChatModel:
         model_version=config.AZURE_CHAT_MODEL,
     )
     return chat_model
+
+
+def make_embeddings() -> Embeddings:
+    embeddings = OpenAIEmbeddings(
+        openai_api_type=config.OPENAI_API_TYPE,
+        openai_api_key=config.OPENAI_API_KEY,
+        openai_api_base=config.OPENAI_API_BASE,
+        openai_api_version=config.OPENAI_API_VERSION,
+        deployment=config.AZURE_DEPLOYMENT,
+        model=config.AZURE_CHAT_MODEL,
+    )
+    return embeddings
